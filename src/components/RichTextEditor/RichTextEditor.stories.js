899db4b81d159dbfefad168573ca6c10d6ebbea7
Ivan Saranchonkau <i.saranchonkau@ozitag.com>
2020-06-11 19:43:33 +0300

    Add RichTextEditor

---
+++ src/components/RichTextEditor/RichTextEditor.stories.js
@@ -0,0 +1,11 @@
+import RichTextEditor from './index';
+
+export default {
+  title: 'RichTextEditor',
+};
+
+export const Default = () => ({
+  components: { RichTextEditor },
+  template: `
+      <rich-text-editor/>`,
+});
