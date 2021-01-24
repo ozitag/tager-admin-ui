3ce2a08523c8da20f92f0bc940b3e45f2e75f6bb
seryozha.baleyko <seryozha.baleyko@gmail.com>
2021-01-24 17:45:08 +0300

    Add useCopyToClipboard hooks, new icon done and copy link in CellName component
    Add the ability to specify subtitle

---
+++ src/hooks/useCopyToClipboard.ts
@@ -0,0 +1,34 @@
+import { onUnmounted, ref, Ref, watch } from '@vue/composition-api';
+
+function useCopyToClipboard(
+  timeout = 300
+): [isCopied: Ref<boolean>, handleCopy: (text: string) => void] {
+  const isCopied = ref<boolean>(false);
+
+  function handleCopy(text: string): void {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        isCopied.value = true;
+      })
+      .catch(console.error);
+  }
+
+  let timeoutId: number | undefined;
+
+  watch(isCopied, (newIsCopied) => {
+    if (newIsCopied) {
+      timeoutId = setTimeout(() => {
+        isCopied.value = false;
+      }, timeout);
+    }
+  });
+
+  onUnmounted(() => {
+    clearTimeout(timeoutId);
+  });
+
+  return [isCopied, handleCopy];
+}
+
+export default useCopyToClipboard;
