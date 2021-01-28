84c3937cffa0ce47abf8a3264351752817e9d009
Sergey Baleyko <seryozha.baleyko@gmail.com>
2021-01-28 10:47:13 +0300

    Add `loading` prop in `CountButton` component (#38)

--- src/components/CountButton/CountButton.stories.js
+++ src/components/CountButton/CountButton.stories.js
@@ -5,38 +5,43 @@ export default { title: 'CountButton' };
 export const Primary = () => ({
   components: { CountButton },
   template: `<div>
-      <count-button variant="primary" count="3"></count-button>
+    <count-button variant="primary" :count="777777777"></count-button>
+    <count-button variant="primary" :count="777777777" :loading="true"></count-button>
   </div>`,
 });
 
 export const PrimaryOutline = () => ({
   components: { CountButton },
   template: `<div>
-      <count-button variant="outline-primary" :count="0">
-        <span>Subcategories</span>
-      </count-button>
+    <CountButton variant="outline-primary" :count="0">
+      <span>Subcategories</span>
+    </CountButton>
+    <CountButton variant="outline-primary" :count="0" :loading="true">
+      <span>Subcategories</span>
+    </CountButton>
   </div>`,
 });
 
 export const Secondary = () => ({
   components: { CountButton },
   template: `<div>
-      <count-button variant="secondary" count="13"></count-button>
+    <CountButton variant="secondary" :count="13"></CountButton>
+    <CountButton variant="secondary" :count="13" :loading="true"></CountButton>
   </div>`,
 });
 
 export const SecondaryOutline = () => ({
   components: { CountButton },
   template: `<div>
-      <count-button variant="outline-secondary" count="7"></count-button>
+    <CountButton variant="outline-secondary" :count="7"></CountButton>
   </div>`,
 });
 
 export const Link = () => ({
   components: { CountButton },
   template: `<div>
-      <count-button variant="primary" href="https://ozitag.com"  count="79">
-          Link to OZiTAG
-      </count-button>
+    <CountButton variant="primary" href="https://ozitag.com" :count="79">
+      Link to OZiTAG
+    </CountButton>
   </div>`,
 });
