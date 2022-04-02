c575a255086849a8a8e468ee6d090a96947ed802
Ivan Saranchonkau <ivan.saranchenkov@gmail.com>
2022-04-02 17:51:55 +0300

    Add FormFieldSelect

---
+++ src/components/FormFieldSelect/FormFieldSelect.stories.ts
@@ -0,0 +1,38 @@
+import { ref, watch } from "vue";
+
+import { brandOptions } from "../../msw/fixtures/brand-options";
+import { BaseOptionType } from "../../typings/common";
+
+import FormFieldSelect from "./FormFieldSelect.vue";
+
+export default { title: "FormFieldSelect" };
+
+const OPTIONS = brandOptions;
+
+export const Default = () => ({
+  name: "DefaultFormFieldSelect",
+  components: { FormFieldSelect },
+  setup() {
+    const selected = ref<BaseOptionType | null>(null);
+    const options = ref<ReadonlyArray<BaseOptionType>>(OPTIONS);
+
+    watch(selected, (selected) => {
+      console.log("selected:", selected);
+    });
+
+    return {
+      selected,
+      options,
+    };
+  },
+  template: `
+    <FormFieldSelect
+      v-model:value="selected"
+      :options="options"
+      name="countries"
+      label="Countries"
+      error="Please, select at least 4 countries"
+      no-options-message="No options"
+    />
+  `,
+});
---
+++ src/components/FormFieldSelect/FormFieldSelect.vue
@@ -0,0 +1,55 @@
+<template>
+  <FormGroup>
+    <InputLabel v-if="Boolean(label)" :for="name">
+      {{ label }}
+    </InputLabel>
+    <ComboBox
+      :id="name"
+      :searchable="searchable"
+      :name="name"
+      v-bind="$attrs"
+    />
+    <FormFieldError v-if="Boolean(error)">{{ error }}</FormFieldError>
+  </FormGroup>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import ComboBox from "../ComboBox";
+import FormGroup from "../FormGroup.vue";
+import FormFieldError from "../FormFieldError";
+import InputLabel from "../InputLabel";
+
+interface Props {
+  searchable: boolean;
+  name: string;
+  label: string;
+  error: string;
+}
+
+export default defineComponent({
+  name: "FormFieldSelect",
+  components: {
+    FormGroup,
+    FormFieldError,
+    InputLabel,
+    ComboBox,
+  },
+  inheritAttrs: false,
+  props: {
+    searchable: {
+      type: Boolean,
+      default: false,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    label: String,
+    error: String,
+  },
+});
+</script>
+
+<style scoped lang="scss"></style>
