a73e6a756a43867c99fba498579fdb3eabc9c8c1
Ivan Saranchonkau <ivan.saranchenkov@gmail.com>
2022-04-17 22:58:45 +0300

    v1.0.0-rc.10 - fix serving css

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,10 @@ function resolvePath(filePath: string) {
 
 /**
  * Source: {@link https://github.com/formik/tsdx/blob/158ee9a69c824b71b62cf987fe943a167f47f936/src/utils.ts#L22-L23}
+ * {@link https://github.com/vitejs/vite/blob/v2.9.5/packages/plugin-vue/src/helper.ts#L1}
  */
-const external = (id: string) => !id.startsWith(".") && !path.isAbsolute(id);
+const external = (id: string) =>
+  !id.startsWith(".") && !path.isAbsolute(id) && !id.startsWith("plugin-vue:");
 
 // https://vitejs.dev/config/
 export default defineConfig({
